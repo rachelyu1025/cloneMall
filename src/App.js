be43d87cfbc68458bf8c2fd2711fe0849cc7d015
EUNBI <rachelyu.1025@gmail.com>
2024-08-20 16:06:15 +0900

    ✨tailwindcss 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div>hi</div>;
+  return <div className=''>"Welcome"</div>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
