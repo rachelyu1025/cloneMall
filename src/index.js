37cddb50f8e375d261b042cc65012b041c0e3105
EUNBI <rachelyu.1025@gmail.com>
2024-08-20 15:54:02 +0900

    ✨ react-router-dom 적용

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailPage from './pages/DetailPage';
+import LoginPage from './pages/LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/detail/:id' element={<DetailPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
