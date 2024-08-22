dc72e5d9e2fccabcdaee979803106fa8564face9
EUNBI <rachelyu.1025@gmail.com>
2024-08-22 23:44:18 +0900

    ✨ 상품 detail page 구현

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 const DetailPage = () => {
-  return <div>"상세페이지입니다~"</div>;
+  const location = useLocation();
+  const item = location?.state;
+
+  return (
+    <div>
+      <img src={item.img} alt={item.title} />
+      <div className='flex flex-col m-2'>
+        <strong>{item.title}</strong>
+        <div>{item.size}</div>
+        <span>{`₩ ${item.price}`}</span>
+        <span>
+          {item['fragrant'].map((el, idx) =>
+            idx < item['fragrant'].length - 1 ? `${el} | ` : el
+          )}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default DetailPage;
