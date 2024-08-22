6e79b2d293ff6d20182cd9330cdf451db9ef697b
EUNBI <rachelyu.1025@gmail.com>
2024-08-23 01:42:35 +0900

    ✨ 검색기능 구현

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
   const [isSearch, setIsSearch] = useState(false);
 
-  const handleSearch = () => {
+  const navigate = useNavigate();
+
+  const handleSearchBtn = () => {
     setIsSearch((prev) => !prev);
+  };
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      const keyword = e.target.value;
+
+      navigate(`/?q=${keyword}`);
+    }
   };
 
   return (
     // <div className='flex md:hidden lg:flex lg:space-x-3'>
     <div className='flex ml-3 lg:space-x-3'>
-      <button onClick={handleSearch}>
+      <button onClick={handleSearchBtn}>
         <MagnifyingGlassIcon className='w-6 h-6 md:mr-2 hover:cursor-pointer lg:mr-0' />
       </button>
 
@@ -20,6 +31,7 @@ const Search = () => {
         <input
           type='text'
           placeholder=' search here!'
+          onKeyDown={(e) => handleSearch(e)}
           className={`w-full border-border_gray border-b-2 focus:outline-none`}
         />
       </div>
--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ProductCard from '../components/ProductCard';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
   // 전체 상품 데이터
   const [data, setData] = useState([]);
+  const [query, setQuery] = useSearchParams();
 
   // api 호출 함수
   const getProducts = async () => {
+    const searchQuery = query.get('q') || '';
+
     await axios
-      .get(process.env.REACT_APP_URL)
+      .get(`${process.env.REACT_APP_URL}?q=${searchQuery}`)
       .then((res) => {
         setData((prev) => res.data);
       })
@@ -19,7 +23,7 @@ const ProductAll = () => {
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
 
   return (
     <div className='grid grid-cols-2 gap-3 md:px-4 lg:grid-cols-3'>
