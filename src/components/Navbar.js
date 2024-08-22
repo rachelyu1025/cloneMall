8f44cb778f5c761ec19e08520fbfd784b6225519
EUNBI <rachelyu.1025@gmail.com>
2024-08-22 18:40:04 +0900

    ✨ Login > 로그인페이지 구현

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,16 @@ import Logo from './Logo';
 // import Menu from './Menu';
 import { UserIcon, ShoppingBagIcon } from '@heroicons/react/24/outline';
 import Search from './Search';
+import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleCheckLogin = () => {
+    // 로그인 여부 파악
+    //로그인되어있지않으면 로그인페이지로 이동
+    navigate('/Login');
+  };
+
   return (
     <div className='relative flex justify-between mx-5 my-3 items-center '>
       {/* logo */}
@@ -12,10 +21,13 @@ const Navbar = () => {
       {/* <Menu /> */}
 
       {/* <div className='hidden md:flex md:space-x-5'> */}
-      <div className='flex space-x-3 md:space-x-5'>
+      <div className='flex space-x-3 md:space-x-5 hover:cursor-pointer'>
         <Search />
         {/* mypage */}
-        <UserIcon className='w-6 h-6 lg:w-8 lg:h-8' />
+        <UserIcon
+          onClick={handleCheckLogin}
+          className='w-6 h-6 lg:w-8 lg:h-8'
+        />
 
         {/* cart */}
         <ShoppingBagIcon className='w-6 h-6 lg:w-8 lg:h-8' />
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginPage = () => {
-  return <div>"로그인하세요~"</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const handleLogin;
+
+  return (
+    <div className='flex w-screen h-[80vh] lg:h-[90vh] lg:justify-center'>
+      <div className='hidden md:flex justify-center items-center w-1/2 h-full'>
+        <img
+          src='https://i.pinimg.com/564x/26/69/d8/2669d87106d692fafd46590df6cfd405.jpg'
+          alt=''
+          className='w-full lg:w-[70%] lg:h-3/4'
+        />
+      </div>
+      <div className='flex flex-col px-[20%] py-[30%] space-y-12 w-full md:w-1/2 md:py-36 md:px-16 lg:px-24'>
+        <strong className='text-xl font-medium'>SIGN IN</strong>
+
+        <form onSubmit={() => {}} className='flex flex-col space-y-8 lg:pr-10'>
+          <div className='border-b-2'>
+            <label htmlFor='username' className='hidden'>
+              UserName
+            </label>
+            <input
+              id='username'
+              type='text'
+              value={username}
+              placeholder=' Username'
+              onChange={(e) => setUsername(e.target.value)}
+              className='w-full focus:outline-none'
+            />
+          </div>
+
+          <div className='border-b-2'>
+            <label htmlFor='password' className='hidden'>
+              PASSWORD
+            </label>
+            <input
+              id='password'
+              type='password'
+              value={password}
+              placeholder=' Password'
+              onChange={(e) => setPassword(e.target.value)}
+              className='w-full focus:outline-none'
+            />
+          </div>
+
+          <button className='self-end md:text-lg'>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
