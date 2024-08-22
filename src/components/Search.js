149d4dba82032460038f004463e670c036822a94
EUNBI <rachelyu.1025@gmail.com>
2024-08-22 16:35:15 +0900

    ✨ Search > input크기 수정

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,16 +11,16 @@ const Search = () => {
 
   return (
     // <div className='flex md:hidden lg:flex lg:space-x-3'>
-    <div className='flex lg:flex lg:space-x-3'>
+    <div className='flex ml-3 lg:space-x-3'>
       <button onClick={handleSearch}>
-        <MagnifyingGlassIcon className='w-6 h-6 mr-2 hover:cursor-pointer lg:mr-0' />
+        <MagnifyingGlassIcon className='w-6 h-6 md:mr-2 hover:cursor-pointer lg:mr-0' />
       </button>
 
-      <div className={`${!isSearch && 'hidden'} lg:inline-block`}>
+      <div className={`${!isSearch && 'hidden'} w-[80%] lg:inline-block`}>
         <input
           type='text'
           placeholder=' search here!'
-          className={`border-border_gray border-b-2 focus:outline-none`}
+          className={`w-full border-border_gray border-b-2 focus:outline-none`}
         />
       </div>
     </div>
